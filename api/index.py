894c76a41d5f0cd94a8137905a57f6a4b67d1ecd
tjahjoe <ntell634@gmail.com>
2026-04-18 02:29:53 +0700

    first commit

--- api/index.py
+++ api/index.py
@@ -1,16 +1,16 @@
 from fastapi import FastAPI
-from mangum import Mangum
 from supabase_client import supabase
 
 app = FastAPI()
 
-@app.get("/")
-def home():
+@app.get("/api")
+def root():
     return {"status": "API running"}
 
-@app.get("/data")
+@app.get("/api/data")
 def get_data():
-    res = supabase.table("maggot_logs").select("*").execute()
-    return res.data
-
-handler = Mangum(app)
+    try:
+        res = supabase.table("maggot_logs").select("*").execute()
+        return {"data": res.data}
+    except Exception as e:
+        return {"error": str(e)}
